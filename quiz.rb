d62ea80658bb9dadaffb94b39ebbd5345b5eac0c
Sam David <sfd1987@gmail.com>
2016-01-18 11:16:18 -0800

    starting counting strands

--- quiz.rb
+++ quiz.rb
@@ -1,6 +1,8 @@
 require 'csv'
 
 class Question
+  attr_reader :strand_id, :strand_name, :standard_id, :standard_name, :question_id, :difficulty
+
   def initialize(args)
     @strand_id = args[:strand_id]
     @strand_name = args[:strand_name]
@@ -40,6 +42,40 @@ def parse_question_csv
   questions
 end
 
-p parse_question_csv
+def build_strand_hash
+
+end
+
+def create_quiz(num_of_questions, questions)
+  strand_count = {}
+  questions.each do |question|
+    if strand_count[question.strand_id] == nil
+      strand_count[question.strand_id] = 1
+    else
+      strand_count[question.strand_id] += 1
+    end
+  end
+  p strand_count
+  current_strand_count = {}
+  strand_count.each do |k,v|
+    if current_strand_count[k] == nil
+      current_strand_count[k] = 0
+    end
+  end
+  p strand_max = questions.length / strand_count.length
+  final_questions = []
+  questions.each do |question|
+    if current_strand_count[question.strand_id] <= current_strand_count.max_by{|k,v| v}[1] && num_of_questions > 0 && current_strand_count[question.strand_id] <= strand_max
+      final_questions.push(question)
+      strand_count[question.strand_id] += 1
+      num_of_questions -= 1
+    end
+  end
+  final_questions
+end
+
+quiz_questions = parse_question_csv
+
+p create_quiz(4, quiz_questions)
 
 # p questions
